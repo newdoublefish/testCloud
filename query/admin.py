5a95d0b962d75f0cf010db46c33288acf0057486
newdoublefish <newdoublefish@163.com>
2018-04-12 21:49:48 +0800

    20180412

--- query/admin.py
+++ query/admin.py
@@ -9,7 +9,11 @@ class StubInfoAdmin(admin.ModelAdmin):
 class RecordAdmin(admin.ModelAdmin):
     list_display=('sn_text','testtype','report_text','pub_date','approved_bool')
     search_fields=('sn_text',)
+
+class BoardInfoAdmin(admin.ModelAdmin):
+    list_display=('board_text','pub_date')
+    search_fields=('board_text',)
 admin.site.register(StubInfo,StubInfoAdmin)
-admin.site.register(BoardInfo)
+admin.site.register(BoardInfo,BoardInfoAdmin)
 #admin.site.register(TestType)
 admin.site.register(Record,RecordAdmin)
